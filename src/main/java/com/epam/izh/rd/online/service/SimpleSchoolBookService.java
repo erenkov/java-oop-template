533cbdbbb66fa71ee41236e79aa9e0faa35903e5
aleksandr.erenkov <al.erenkov@yandex.ru>
2020-04-09 17:37:34 +0400

    Созданы entity, repository - без реализации,  service- без реализации.

--- src/main/java/com/epam/izh/rd/online/service/SimpleSchoolBookService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleSchoolBookService.java
@@ -2,25 +2,20 @@ package com.epam.izh.rd.online.service;
 
 import com.epam.izh.rd.online.entity.Author;
 import com.epam.izh.rd.online.entity.Book;
+import com.epam.izh.rd.online.entity.SchoolBook;
+import com.epam.izh.rd.online.repository.BookRepository;
 
-/**
- * Интерфейс сервиса для выполнения бизнес логики при работе с книга и авторами и взаимодействием с
- * репозиторием для книг BookRepository и сервисом для авторов AuthorService.
- * <p>
- * Необходимо:
- * 1) Создать в этом же пакете класс SimpleSchoolBookService
- * 2) Имплементировать им данный интерфейс
- * 3) Добавить все методы (пока можно не писать реализацию)
- * 4) Добавить в SimpleSchoolBookService приватное поле "BookRepository<SchoolBook> schoolBookBookRepository" - это репозиторий
- * книг к которому вы будете обращаться в методах
- * 5) Добавить в SimpleSchoolBookService приватное поле "AuthorService authorService" - это сервис для работы с авторами к которому
- * вы будете обращаться в методах
- * 6) Создать дефолтный конструтор (без параметров)
- * 7) Создать конструтор с параметрами "BookRepository<SchoolBook> schoolBookBookRepository, AuthorService authorService"
- * (который будет устанвливать в поле schoolBookBookRepository и в поле authorService значения)
- * 8) Написать в классе SimpleSchoolBookService реализацию для всех методов
- */
-public interface BookService<T extends Book> {
+public class SimpleSchoolBookService implements BookService {
+    private BookRepository<SchoolBook> schoolBookBookRepository;
+    private AuthorService authorService;
+
+    public SimpleSchoolBookService() {
+    }
+
+    public SimpleSchoolBookService(BookRepository<SchoolBook> schoolBookBookRepository, AuthorService authorService) {
+        this.schoolBookBookRepository = schoolBookBookRepository;
+        this.authorService = authorService;
+    }
 
     /**
      * Метод должен сохранять книгу.
@@ -34,33 +29,48 @@ public interface BookService<T extends Book> {
      * <p>
      * Соответственно, если книга была успешно сохранена - метод возвращает true, если же книга не была сохранена - метод возвращает false.
      */
-    boolean save(T book);
+    @Override
+    public boolean save(Book book) {
+        return false;
+    }
 
     /**
      * Метод должен находить книгу по имени.
      * <p>
      * По факту, он просто обращается к репозиторию с книгами и вызывает аналогичный метод, псоле чего возвращает результат.
      */
-    T[] findByName(String name);
+    @Override
+    public Book[] findByName(String name) {
+        return new Book[0];
+    }
 
     /**
      * Метод должен находить количество сохраненных книг по конкретному имени книги.
      */
-    int getNumberOfBooksByName(String name);
+    @Override
+    public int getNumberOfBooksByName(String name) {
+        return 0;
+    }
 
     /**
      * Метод должен удалять все книги по имени.
      * <p>
      * По факту, он просто обращается к репозиторию с книгами и вызывает аналогичный метод, псоле чего возвращает результат.
      */
-    boolean removeByName(String name);
+    @Override
+    public boolean removeByName(String name) {
+        return false;
+    }
 
     /**
      * Метод должен возвращать количество всех книг.
      * <p>
      * По факту, он просто обращается к репозиторию с книгами и вызывает аналогичный метод, псоле чего возвращает результат.
      */
-    int count();
+    @Override
+    public int count() {
+        return 0;
+    }
 
     /**
      * Метод должен возвращать автора книги по названию книги.
@@ -69,5 +79,8 @@ public interface BookService<T extends Book> {
      * <p>
      * Если такой книги не найдено, метод должен вернуть null.
      */
-    Author findAuthorByBookName(String name);
+    @Override
+    public Author findAuthorByBookName(String name) {
+        return null;
+    }
 }
